9eb456eff0dcab92836ec74e8a4ecaac8410db32
Mateus <mmfrb@cin.ufpe.br>
2016-06-02 09:15:21 -0300

    checking number of arguments

--- apps/client/lib/client/proxy.ex
+++ apps/client/lib/client/proxy.ex
@@ -2,10 +2,14 @@ defmodule Client.Proxy do
 
   def generate_function({{host, port}, {functionName, args_checker}}) do
     fn args ->
-      if type_check(args, args_checker) do
-        {:ok, Client.Requestor.invoke({host, port}, functionName, args)}
+      if length(args) != length(args_checker) do
+        {:error, :invalid_number_of_arguments}
       else
-        {:error, :invalid_arguments}
+        if type_check(args, args_checker) do
+          {:ok, Client.Requestor.invoke({host, port}, functionName, args)}
+        else
+          {:error, :invalid_argument_types}
+        end
       end
     end
   end
