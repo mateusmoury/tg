8de697563e699199ddb8854ada3a957e906e905b
Mateus <mmfrb@cin.ufpe.br>
2016-06-02 09:09:31 -0300

    improving proxy type checker

--- apps/client/lib/client/proxy.ex
+++ apps/client/lib/client/proxy.ex
@@ -1,8 +1,19 @@
 defmodule Client.Proxy do
 
-  def generate_function({{host, port}, functionName}) do
+  def generate_function({{host, port}, {functionName, args_checker}}) do
     fn args ->
-      Client.Requestor.invoke({host, port}, functionName, args)
+      if type_check(args, args_checker) do
+        {:ok, Client.Requestor.invoke({host, port}, functionName, args)}
+      else
+        {:error, :invalid_arguments}
+      end
     end
+  end
+
+  defp type_check([], []), do: true
+  defp type_check([], [_hd | _]), do: false
+  defp type_check([_hd | _], []), do: false
+  defp type_check([arg | tail_args], [arg_checker | tail_args_checker]) do
+    arg_checker.(arg) && type_check(tail_args, tail_args_checker)
   end
 end
