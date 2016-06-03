129467b3b55b61105cf7587a56e700bda3ab72e6
Mateus <mmfrb@cin.ufpe.br>
2016-06-03 10:09:28 -0300

    binding services to naming service

--- apps/server/lib/server.ex
+++ apps/server/lib/server.ex
@@ -4,6 +4,7 @@ defmodule Server do
   def start(_type, _args) do
     result = {:ok, sup} = Supervisor.start_link(__MODULE__, [])
     start_workers(sup)
+    bind_services
     result
   end
 
@@ -11,6 +12,13 @@ defmodule Server do
     {:ok, invoker_sup} =
       Supervisor.start_child(sup, supervisor(Task.Supervisor, []))
     Supervisor.start_child(sup, worker(Task, [InvocationLayer.Invoker, :invoke, [4040, invoker_sup]]))
+  end
+
+  def bind_services do
+    naming_service_address = {:localhost, 5050}
+    naming_service_bind = {NamingService.LookupTable, :bind, [&is_bitstring/1, &is_tuple/1]}
+    remote_bind = InvocationLayer.ClientProxy.generate_function({naming_service_address, naming_service_bind})
+    remote_bind.(["add", {{:localhost, 4040}, {Server.Application, :add, [&is_number/1, &is_number/1]}}])
   end
 
   def init(_) do
